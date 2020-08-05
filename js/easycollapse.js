5c7d58448b0037091d0d06249b2607fbd5e153d6
stewa75 <stemura@gmail.com>
2020-08-05 17:01:19 +0200

    Add files via upload

--- js/easycollapse.js
+++ js/easycollapse.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
 	//************************************************
 	// Capture click on Topic Title and show topic div
 
-	$( ".format_easycollapsible_fheader" ).each( function() {
+	$( ".format-easycollapsible_fheader" ).each( function() {
 
 		$(this).on("click", function (e) {	
 
 			// Toogle the class
-			$( $(this).attr("href") ).toggleClass( "showed" );
-			$( $(this) ).toggleClass( "showed" );
-			$( ".multi-collapse" ).not($(this).attr("href")).removeClass( "showed" );
+			$( $(this).attr("href") ).toggleClass( "format-easycollapsible_showed" );
+			$( $(this) ).toggleClass( "format_easycollapsible_showed" );
+			$( ".format-easycollapsible_multi-collapse" ).not($(this).attr("href")).removeClass( "format-easycollapsible_showed" );
 
 			// Center the page on the element clicked
 			var el = $( e.target.getAttribute('href') );
@@ -44,7 +44,7 @@ $(document).ready(function(){
 
 	$("#showhideallbtn").on("click", function () {
 
-		$( ".multi-collapse" ).toggleClass( "showed" );
+		$( ".format-easycollapsible_multi-collapse" ).toggleClass( "format-easycollapsible_showed" );
 
 	});
 
