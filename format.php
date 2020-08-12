90fab695e62d353e57cf99bdad13b97964637a05
stewa75 <stemura@gmail.com>
2020-08-12 12:04:50 +0200

    Add files via upload

--- format.php
+++ format.php
@@ -13,7 +13,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /*
  *
  * @package    format_easycollapsible
@@ -24,42 +23,27 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License
  *
  */
-
 defined('MOODLE_INTERNAL') || die();
-
-require_once($CFG->libdir.'/filelib.php');
-require_once($CFG->libdir.'/completionlib.php');
-
-
+require_once($CFG->libdir . '/filelib.php');
+require_once($CFG->libdir . '/completionlib.php');
 if ($topic = optional_param('topic', 0, PARAM_INT)) {
     $url = $PAGE->url;
     $url->param('section', $topic);
     debugging('Outdated topic param passed to course/view.php', DEBUG_DEVELOPER);
     redirect($url);
 }
-
 $context = context_course::instance($course->id);
-
 if (($marker >= 0) && has_capability('moodle/course:setcurrentsection', $context) && confirm_sesskey()) {
     $course->marker = $marker;
     course_set_marker($course->id, $marker);
 }
-
 $course = course_get_format($course)->get_course();
-
 course_create_sections_if_missing($course, range(0, $course->numsections));
-
 $renderer = $PAGE->get_renderer('format_easycollapsible');
-
-
 if (!empty($displaysection)) {
     $renderer->print_single_section_page($course, null, null, null, null, $displaysection);
-	
 } else {
-	
     $renderer->print_multiple_section_page($course, null, null, null, null);
-	
 }
-
 /// Include course format js module
 $PAGE->requires->js('/course/format/topics/format.js');
